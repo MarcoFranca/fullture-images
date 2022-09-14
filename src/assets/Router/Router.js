f6a19da646b67898548db8a968cf2cd989d4237a
Marco Franca <dev.marcotullio@gmail.com>
2022-09-14 11:36:38 -0300

    import from 404 error page

--- src/assets/Router/Router.js
+++ src/assets/Router/Router.js
@@ -2,14 +2,15 @@ import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Index from "../../pages/Index";
 import Search from "../../pages/Search/Search";
+import PageNotFound from "../../pages/pageNotFound";
 
 export default function Router() {
     return(
         <BrowserRouter>
             <Routes>
                 <Route index element={<Index/>}/>
-                <Route path='/search/:params' element={<Search/>}/>
-                <Route path='*' element={<div>Page is not found - 404</div>}/>
+                <Route path='/search/' element={<Search/>}/>
+                <Route path='*' element={<PageNotFound />}/>
             </Routes>
         </BrowserRouter>
     )
