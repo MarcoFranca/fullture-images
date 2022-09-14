df5e24475da47bfecb838246bd22d852c097c1ab
Marco Franca <dev.marcotullio@gmail.com>
2022-09-14 11:37:37 -0300

    create header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './Header.css'
+import logoGoogle from '../../assets/image/Google_2015_logo.svg'
 
 export default function Header() {
     return (
         <nav>
-            <img src="" alt="logo"/>
+            <img src={logoGoogle} alt="logo"/>
             <input type="text" placeholder="procurar imagens"/>
             <button>Buscar</button>
         </nav>
