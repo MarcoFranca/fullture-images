e9192c77143b23dcd9616e02550a4aac8d461a15
Marco Franca <dev.marcotullio@gmail.com>
2022-09-16 20:27:31 -0300

    useEffect

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import logoGoogle from '../../assets/image/Google_2015_logo.svg'
 import {Main, Search} from "./IndexStyle";
 import {useGlobal} from "../../assets/global/contex/globalState";
+import {getImg} from "../../assets/apis/req";
+import {useNavigate} from "react-router-dom";
 
 export default function Index() {
     const {states, setters} = useGlobal()
     const {input} = states
     const {setInput} = setters
+    const {setImages} = setters
+    const navigate = useNavigate()
+    const [test, setTest] = useState()
+
+    useEffect(()=>{
+        setInput("")
+    },[])
+
+    const onSearch = (e) => {
+        e.preventDefault();
+        navigate(`/search/${input}`);
+        getImg(setImages,input)
+    }
+
 
     return (
         <Main >
